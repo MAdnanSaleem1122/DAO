fd4e118d3e6ac2b22cdb68c9628e4d243ec807d0
Muhammad Adnan <adnanbest1122@gmail.com>
2023-01-12 12:16:32 +0500

    Build DAO

---
+++ hardhat-tutorial/constants/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xB460Df55e53E312F9f54B54CAAFb8129adec0dDC";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
